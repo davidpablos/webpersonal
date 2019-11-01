e0a7947582e85e44bd29eacf1c91413a55353ebf
David Pablos <david.pablos@gmail.com>
2019-11-02 00:55:36 +0100

    Personal web made with Django

---
+++ webpersonal/portfolio/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class Project(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Título")
+    description = models.TextField(verbose_name="Descripción")
+    image = models.ImageField(verbose_name="Imagen", upload_to="projects")
+    more_info = models.URLField(null=True, blank=True, verbose_name="Más información")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+
+    class Meta:
+        verbose_name = "Proyecto"
+        verbose_name_plural = "Proyectos"
+        ordering = ["-created_at"]
+    
+    def __str__(self):
+        return self.title
